0bd49927226edf2333899d9be45f153a38aa1167
Kshitij Khule <kshitijg.khule@gmail.com>
2026-04-04 18:56:16 +0530

    Changed baseURL in api.js to ec2 public-ip

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "http://13.200.253.81:80/api"
 });
 
 API.interceptors.request.use((req) => {
