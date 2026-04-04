4fe8af2ae835009a628523b72a35f0a97d493373
Kshitij Khule <kshitijg.khule@gmail.com>
2026-04-04 16:45:54 +0530

    fix: disable SSL for RDS connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: false
 });
 
 module.exports = pool;
